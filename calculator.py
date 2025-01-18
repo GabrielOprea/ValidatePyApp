0ee9e163f656d966b2400da34562fb3e6331f616
marinasimionescu21 <marina.simionescu21@gmail.com>
2025-01-19 00:52:03 +0200

    add div functionality so that the calculator class can perform division on some float numbers

--- calculator.py
+++ calculator.py
@@ -13,3 +13,18 @@ class Calculator:
     def multiply(self, a, b):
         """Returns the product of a and b."""
         return a * b
+        
+
+        # This function will perform division
+    def diVide(self, the_var_to_be_divided, someVarFor_diViding):
+            #Returns the quotient of a and b.
+                    # Raises an error if
+                        # dividing by zero.
+                        # This should work for any someVarFor_diViding that is different from 0. If the var is zero then we cannot perform the operation. In that case an error should be thrown since it is impossible to perform division due to Math's laws.
+                if someVarFor_diViding == 0:
+                    #Should not be   posible
+                    raise ValueError("Cannot divide by zero")
+                return the_var_to_be_divided / someVarFor_diViding
+    
+calc = Calculator()
+print(calc.diVide(4, 2))
