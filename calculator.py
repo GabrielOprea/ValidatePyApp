f2c256c60aad42efd7ee04601e79654772f48a1b
marinasimionescu21 <marina.simionescu21@gmail.com>
2025-01-19 00:46:40 +0200

    Feat: Addition and substraction

---
+++ calculator.py
@@ -0,0 +1,11 @@
+class Calculator:
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        """Returns the sum of a and b."""
+        return a + b
+
+    def subtract(self, a, b):
+        """Returns the difference of a and b."""
+        return a - b
